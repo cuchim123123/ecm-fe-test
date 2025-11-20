53ba6190f172a7dd1e61d483c3a1480314e19d47
cuchim <ngbao2485@gmail.com>
2025-11-21 00:09:26 +0700

    Add final responsive improvements for mobile and small screens

--- test/src/components/Layout.jsx
+++ test/src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
     return (
         <div className='min-h-screen overflow-x-hidden w-full'>
             <div className='w-full shadow-sm border-b border-gray-100'>
-                <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+                <div className='px-2 sm:px-4 md:px-[5vw] lg:px-[7vw] xl:px-[9vw]'>
                     <Navbar />
                 </div>
             </div>

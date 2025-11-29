07da0df49b169c44b3e6cf03a1a79a7805a41cc7
cuchim <ngbao2485@gmail.com>
2025-11-29 19:50:42 +0700

    feat: improve Layout with flex structure and footer

--- test/src/components/Layout.jsx
+++ test/src/components/Layout.jsx
@@ -6,12 +6,22 @@ import { Toaster } from './ui/sonner'
 
 const Layout = () => {
     return (
-        <div className='overflow-x-hidden w-full'>
+        <div className='min-h-screen flex flex-col overflow-x-hidden w-full'>
             <Navbar />
             
-            <main className='w-full relative pt-[73px]'>
+            <main className='flex-1 w-full relative pt-[73px] md:pt-[80px]'>
                 <Outlet />
             </main>
+            
+            {/* Footer placeholder - add your footer component here */}
+            <footer className='w-full bg-slate-900 text-white py-8 mt-auto'>
+                <div className='max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8'>
+                    <div className='text-center text-sm text-slate-400'>
+                        © 2025 MilkyBloom Toy Store. All rights reserved.
+                    </div>
+                </div>
+            </footer>
+            
             <Toaster />
         </div>
     )
